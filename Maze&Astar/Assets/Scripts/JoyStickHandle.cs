38423cc2c3280f78fc83c0cdb368a29626259f6f
qkrgustn <sdh210212@sdh.hs.kr>
2022-11-25 22:55:50 +0900

    (코로나, hku이슈)주석 달기

--- Maze&Astar/Assets/Scripts/JoyStickHandle.cs
+++ Maze&Astar/Assets/Scripts/JoyStickHandle.cs
@@ -26,24 +26,30 @@ public class JoyStickHandle : MonoBehaviour
         StartCoroutine(JoystickDirection());
     }
 
+    //조이스틱이 어느 방향인지 알아내고 그 방향대로 움직이게 하는 코루틴
     public IEnumerator JoystickDirection()
     {
+        //조이스틱이 드래그 중이면
         if (JS.isDrag)
         {
+            #region UI 레이캐스트
             m_PointerEventData = new PointerEventData(m_EventSystem);
-
-            //m_PointerEventData.position = Input.mousePosition;
             Camera.main.ScreenToWorldPoint(transform.position);
             m_PointerEventData.position = Camera.main.WorldToScreenPoint(transform.position);
             List<RaycastResult> results = new List<RaycastResult>();
             m_Raycaster.Raycast(m_PointerEventData, results);
+            #endregion
+            //레이캐스트에 맞은 오브젝트들이 1개 이상이면
             if (results.Count > 0)
             {
+                //맞은 오브젝트들 갯수만큼 for
                 for (int i = 0; i < results.Count; i++)
                 {
+                    //오브젝트 이름들이 스틱, 조이스틱이 아니면
                     if (results[i].gameObject.name != "Stick" &&
                         results[i].gameObject.name != "Joystick")
                     {
+                        //그 방향대로 플레이어를 움직인다
                         player.PlayerMove(results[i].gameObject.name);
                     }
 
--- Maze&Astar/Assets/Scripts/MazeCreate.cs
+++ Maze&Astar/Assets/Scripts/MazeCreate.cs
@@ -18,38 +18,46 @@ public class MazeCreate : MonoBehaviour
             {
                 if(i % 2 == 0 || j % 2 == 0)
                 {
+                    //격자무늬로 벽들을 생성함
                     ObjPool.GetObject(EPoolType.Wall, new Vector2(MazeStartingPoint.transform.position.x + i, MazeStartingPoint.transform.position.y + j));
                 }
             }
         }
+
         for (int i = 0; i < Size; i++)
         {
             for (int j = 0; j < Size; j++)
             {
+                //두줄마다 x, y축 다 살려놓고 랜덤하게 뚫는거기 때문에 건너뛰어줘야함
                 if (i % 2 == 0 || j % 2 == 0)
-                    continue;
+                    //continue;
 
                 if(i == Size - 2)
                 {
+                    //마지막 부분 y축 선 뚫어줌
                     ObjPool.GetObject(EPoolType.DeleteWall, new Vector2(MazeStartingPoint.transform.position.x + i, MazeStartingPoint.transform.position.y + j + 1));
                     continue;
                 }
                 if(j == Size - 2)
                 {
+                    //마지막 부분 x축 선 뚫어줌
                     ObjPool.GetObject(EPoolType.DeleteWall, new Vector2(MazeStartingPoint.transform.position.x + i + 1, MazeStartingPoint.transform.position.y + j));
                     continue;
                 }
 
                 if(Random.Range(0, 2) == 0)
                 {
+                    //랜덤하게 x축 기준으로 뚫어줌
                     ObjPool.GetObject(EPoolType.DeleteWall, new Vector2(MazeStartingPoint.transform.position.x + i, MazeStartingPoint.transform.position.y + j + 1));
                 }
                 else
                 {
+                    //랜덤하게 y축 기준으로 뚫어줌
                     ObjPool.GetObject(EPoolType.DeleteWall, new Vector2(MazeStartingPoint.transform.position.x + i + 1, MazeStartingPoint.transform.position.y + j));
                 }
             }
         }
+        //시작지점 뚫어줌
         ObjPool.GetObject(EPoolType.DeleteWall, new Vector2(MazeStartingPoint.transform.position.x + 1, MazeStartingPoint.transform.position.y));
     }
 }
