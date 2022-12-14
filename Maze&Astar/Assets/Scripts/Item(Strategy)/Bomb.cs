2259f4a44a29c870114620f176f82bc101839bcc
qkrgustn <sdh210212@sdh.hs.kr>
2022-12-14 20:26:06 +0900

    폭탄 기능 완전히 구현(연출 X)

--- Maze&Astar/Assets/Scripts/Item(Strategy)/Bomb.cs
+++ Maze&Astar/Assets/Scripts/Item(Strategy)/Bomb.cs
@@ -6,12 +6,18 @@ public class Bomb : MonoBehaviour, I_Item
 {
     public void UseItem()
     {
-        for (int i = 0; i < 3; i++)
+        for (int i = -1; i < 2; i++)
         {
-            for (int j = 0; j < 3; j++)
+            for (int j = -1; j < 2; j++)
             {
-                MazeCreate.Instance.Blocks[Player.Instance.PlayerPosX + j - 1, Player.Instance.PlayerPosY + i - 1] = false;
-                ObjPool.GetObject(EPoolType.DeleteWall, new Vector3(Player.Instance.PlayerPosX + j - 1, Player.Instance.PlayerPosY + i - 1));
+                if (Player.Instance.PlayerPosX + j > 0 &&
+                    Player.Instance.PlayerPosY + i > 0 &&
+                    Player.Instance.PlayerPosX + j != MazeCreate.Instance.MazeSize &&
+                    Player.Instance.PlayerPosY + i != MazeCreate.Instance.MazeSize)
+                {
+                    MazeCreate.Instance.Blocks[Player.Instance.PlayerPosX + j, Player.Instance.PlayerPosY + i] = false;
+                    ObjPool.GetObject(EPoolType.DeleteWall, new Vector3(Player.Instance.PlayerPosX + j, Player.Instance.PlayerPosY + i));
+                }
             }
         }
         Debug.Log("Boom!!!!!!");
