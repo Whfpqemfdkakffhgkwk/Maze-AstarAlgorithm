a87988b4de9e1015f8fdc0936363b9d088487f1f
qkrgustn <sdh210212@sdh.hs.kr>
2022-12-19 20:30:27 +0900

    아 astar 어렵다 아직 개발중
    모든 노드로 바꾼다음에 점점 노드가 진행되는 방식으로 해야할듯

--- Maze&Astar/Assets/Scripts/Boss.cs
+++ Maze&Astar/Assets/Scripts/Boss.cs
@@ -3,8 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[System.Serializable]
+public class Node
+{
+    [Tooltip("이동했던 거리")]public int G; 
+    [SerializeField, Tooltip("목표까지의 거리")]public int H;
+    
+    //G + H, 이동했던거리 + 목표까지의 거리
+    public int F { get { return G + H; } }
+}
+
 public class Boss : MonoBehaviour
 {
+    private int CurPosX = 1, CurPosY = 0;
+    [Tooltip("0위, 1아래, 2왼 3오")]Node[] nodes;
+
+    private void Start()
+    {
+        HCount();
+    }
+    void HCount()
+    {
+        nodes[0].H = Mathf.Abs(CurPosX - Player.Instance.PlayerPosX) + Mathf.Abs(CurPosY + 1 - Player.Instance.PlayerPosY);
+        nodes[1].H = Mathf.Abs(CurPosX - Player.Instance.PlayerPosX) + Mathf.Abs(CurPosY - 1 - Player.Instance.PlayerPosY);
+        nodes[2].H = Mathf.Abs(CurPosX - 1 - Player.Instance.PlayerPosX) + Mathf.Abs(CurPosY - Player.Instance.PlayerPosY);
+        nodes[3].H = Mathf.Abs(CurPosX + 1 - Player.Instance.PlayerPosX) + Mathf.Abs(CurPosY - Player.Instance.PlayerPosY);
+    }
     public void Move(int name)
     {
         switch (name)
